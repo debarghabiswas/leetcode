a3e3c9463a784653c3208b92df3e1c5ca5aeaf42
debarghabiswas <debarghab2005@gmail.com>
2024-08-03 10:37:21 +0530

    ROMAN TO INTEGER

---
+++ EASY/ROMAN TO INTEGER/romanToInteger.java
@@ -0,0 +1,8 @@
+// import java.util.*;
+// class romanToInteger {
+//     public int romanToInt(String s){
+//         return 0;
+//     }
+//     public static void main(String[] args) {
+//     }
+// }
---
+++ EASY/ROMAN TO INTEGER/roman_to_integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s) -> int:
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        total = 0
+        for i in range(len(s)):
+            if i+1 < len(s) and roman[s[i]]<roman[s[i+1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+        return total
+
+ob = Solution()
+print(ob.romanToInt("MCMXCIV"))  # Output: 1994
