a14b881ee2a2a318b2f8161f8ed5b14cc3c16bb2
debarghabiswas <debarghab2005@gmail.com>
2024-08-05 19:35:07 +0530

    medium difficulty

---
+++ MEDIUM/Longest Substring Without Repeating Characters/longest_substring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s) -> int:
+        char_set = set()
+        start = 0
+        max_length = 0
+        for end in range(len(s)):
+            while s[end] in char_set:
+                char_set.remove(s[start])
+                start += 1
+            char_set.add(s[end])
+            max_length = max(max_length, end-start+1)
+        return max_length
+
+ob = Solution()
+s = 'abcabcbb'
+print(ob.lengthOfLongestSubstring(s))
